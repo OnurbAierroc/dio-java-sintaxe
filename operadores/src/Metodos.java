c1c976a39e9cc9af614cef5e2804737a8ef55200
Bruno <bruno.cs1001@gmail.com>
2023-12-15 11:03:37 -0300

    Explicação de Metodos

---
+++ operadores/src/Metodos.java
@@ -0,0 +1,25 @@
+public class Metodos {
+    public static void main (String [] args){
+        // Métodos são funçoes / sub-rotinas
+        // Deve ser nomeados como Verbos
+        // nomenclatura = camelCase calcularImposto
+
+        /*
+        
+        public double somar(int n1, int n2){
+                Código
+            return ...;
+
+        public void imprimir(){
+            Código
+            Não precisa ter return
+        }
+
+        public double (int dividendo, int divisor) throws Exepction {} -- Trabalha com excessão
+
+        private void metodoPrivado (){} -- Visibilidade privada, diferente do public
+        
+         */
+    }
+    
+}
